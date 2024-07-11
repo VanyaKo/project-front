bb5e57c0a3c9f5d839c8a3cc08d841d5f3905887
VanyaKo <thedarkknight.vanerk@gmail.com>
2024-07-11 21:57:32 +0500

    extract render logic

--- src/main/webapp/js/my.js
+++ src/main/webapp/js/my.js
@@ -2,14 +2,7 @@ const PLAYERS_URL = '/rest/players'
 
 function showButtons(pages) {
     $('button.unselected-paging-button').remove()
-    for (let i = 0; i < pages; i++) {
-        const buttonTag = `<button>${i + 1}</button>`
-        const button = $(buttonTag)
-            .attr('id', 'paging-button-' + i)
-            .attr('onclick', `showList(${i})`)
-            .addClass('unselected-paging-button')
-        $('#paging-buttons').append(button)
-    }
+    renderPagingButtons(pages);
 }
 
 function markCurrentPagingButton(pageNumber) {
@@ -29,26 +22,7 @@ function showList(pageNumber) {
                 showList(--pageNumber)
             }
             $.each(response, function (i, user) {
-                $('<tr>').html(
-                    `<td>${user.id}</td>`
-                    + `<td>${user.name}</td>`
-                    + `<td>${user.title}</td>`
-                    + `<td>${user.race}</td>`
-                    + `<td>${user.profession}</td>`
-                    + `<td>${user.level}</td>`
-                    + `<td>${new Date(user.birthday).toLocaleDateString()}</td>`
-                    + `<td>${user.banned}</td>`
-                    + '<td>'
-                    + `<button id="edit-button-${user.id}" onclick="editUser(${user.id})">`
-                    + '<img src="/img/edit.png" alt="edit">'
-                    + '</button>'
-                    + '</td>'
-                    + '<td>'
-                    + `<button id="delete-button-${user.id}" onclick="deleteUser(${user.id})">`
-                    + '<img src="/img/delete.png" alt="delete">'
-                    + '</button>'
-                    + '</td>'
-                ).appendTo('#admin-table')
+                renderTableRaw(user);
             });
         }
     )
@@ -88,39 +62,7 @@ function editUser(id) {
     const deleteButtonId = `#delete-button-${id}`
 
     $(deleteButtonId).remove()
-
-    const saveImageTag = '<img src="/img/save.png" alt="save">'
-    $(editButtonId).html(saveImageTag)
-
-    const currentTrElement = $(editButtonId).parent().parent()
-    const children = currentTrElement.children()
-
-    const tdName = children[1]
-    tdName.innerHTML = `<input id="input-name-${id}" type="text" value="${tdName.innerHTML}">`
-
-    const tdTitle = children[2]
-    tdTitle.innerHTML = `<input id="input-title-${id}" type="text" value="${tdTitle.innerHTML}">`
-
-    const tdRace = children[3]
-    const raceId = '#select-race-' + id
-    const raceCurrentValue = tdRace.innerHTML
-    tdRace.innerHTML = getDropdownHtml(id, 'race', ['HUMAN', 'DWARF', 'ELF', 'GIANT', 'ORC', 'TROLL', 'HOBBIT'])
-    $(raceId).val(raceCurrentValue).change()
-
-    const tdProfession = children[4]
-    const professionId = '#select-profession-' + id
-    const professionCurrentValue = tdProfession.innerHTML
-    tdProfession.innerHTML = getDropdownHtml(id, 'profession', ['WARRIOR', 'ROGUE', 'SORCERER', 'CLERIC', 'PALADIN', 'NAZGUL', 'WARLOCK', 'DRUID'])
-    $(professionId).val(professionCurrentValue).change()
-
-    const tdBanned = children[7]
-    const bannedId = '#select-banned-' + id
-    const bannedCurrentValue = tdBanned.innerHTML
-    tdBanned.innerHTML = getDropdownHtml(id, 'banned', [false, true])
-    $(bannedId).val(bannedCurrentValue).change()
-
-    const propertySaveTag = `saveUser(${id})`
-    $(editButtonId).attr('onclick', propertySaveTag)
+    renderEditRaw(editButtonId, id);
 }
 
 function createUser() {
@@ -185,15 +127,6 @@ function saveUser(id) {
             showList(getCurrentPage())
         }
     })
-}
-function getDropdownHtml(id, name, values) {
-    const nameId = `select-${name}-` + id;
-    let dropdownHtml = `<label for="${name}"></label>` + `<select id="${nameId}" name=${name}>`;
-
-    values.map(function (value) {
-        dropdownHtml += `<option value="${value}">${value}</option>`
-    })
-    return dropdownHtml + '</select>'
 }
 
 function getCurrentPage() {
